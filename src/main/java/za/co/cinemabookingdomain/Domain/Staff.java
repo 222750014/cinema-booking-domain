36431700266f138893f13d18f276454254361cd3
sinokhanyontantiso <sinokhanyontantiso@gmail.com>
2025-05-11 16:03:47 +0200

    am done with staff and concession

---
+++ src/main/java/za/co/cinemabookingdomain/Domain/Staff.java
@@ -0,0 +1,66 @@
+package za.co.cinemabookingdomain.Domain;
+
+public class Staff {
+    private final String name;
+    private final String role;
+    private final String employeeId;
+
+    private Staff(Builder builder) {
+        this.name = builder.name;
+        this.role = builder.role;
+        this.employeeId = builder.employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public static class Builder {
+        private String name;
+        private String role;
+        private String employeeId;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setRole(String role) {
+            this.role = role;
+            return this;
+        }
+
+        public Builder setEmployeeId(String employeeId) {
+            this.employeeId = employeeId;
+            return this;
+        }
+        public Builder copy(Staff staff) {
+            this.name = staff.getName();
+            this.role = staff.getRole();
+            this.employeeId = staff.getEmployeeId();
+            return this;
+        }
+
+        public Staff build() {
+
+            return new Staff(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                '}';
+    }
+}

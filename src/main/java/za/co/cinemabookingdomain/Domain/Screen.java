0e68f34cc4b56742d8fd7091ef6e7f80c80f3f0a
Vuyolwethu27 <222875437@mycput.ac.za>
2025-05-11 21:10:31 +0200

    I'm am done with my entities

---
+++ src/main/java/za/co/cinemabookingdomain/Domain/Screen.java
@@ -0,0 +1,62 @@
+package za.co.cinemabookingdomain.Domain;
+
+/*Vuyolwethu Piyo
+222875437
+
+ */
+public class Screen {
+    private String type;
+    private int capacity;
+    private int screenNumber;  //
+
+    private Screen(Builder builder) {
+        this.type = builder.type;
+        this.capacity = builder.capacity;
+        this.screenNumber = builder.screenNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getScreenNumber() {
+        return screenNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Screen Type: " + type + ", Capacity: " + capacity + ", Screen Number: " + screenNumber;
+    }
+
+
+    public static class Builder {
+        private String type;
+        private int capacity;
+        private int screenNumber;
+
+        public Builder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setCapacity(int capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public Builder setScreenNumber(int screenNumber) {
+            this.screenNumber = screenNumber;
+            return this;
+        }
+
+        public Screen build() {
+            return new Screen(this);
+        }
+    }
+}
+
+

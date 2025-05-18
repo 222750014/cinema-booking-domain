e9a9c357616c5e69a5e3f000b6adff805ca4ecd5
222750014 <222750014@mycput.ac.za>
2025-05-18 18:48:53 +0200

    Payment DOMAIN update and ShowtimeFactory and test

---
+++ src/main/java/za/co/cinemabookingdomain/factory/ShowtimeFactory.java
@@ -0,0 +1,35 @@
+package za.co.cinemabookingdomain.factory;
+
+import za.co.cinemabookingdomain.Domain.Showtime;
+
+import java.time.LocalDate;
+
+public class ShowtimeFactory {
+    public static Showtime CreateShowtime(String movie, String screen, LocalDate date, String format, String language){
+        if (movie == null || movie.isEmpty()) {
+            return null;
+        }
+        if (screen == null || screen.isEmpty()) {
+            return null;
+        }
+        if (date == null) {
+            return null;
+        }
+
+        if (format == null || format.isEmpty()) {
+            return null;
+        }
+        if (language == null || language.isEmpty()) {
+            return null;
+        }
+
+
+        return new Showtime.Builder()
+                .setMovie(movie)
+                .setScreen(screen)
+                .setDate(date)
+                .setFormat(format)
+                .setLanguage(language)
+                .build();
+    }
+}

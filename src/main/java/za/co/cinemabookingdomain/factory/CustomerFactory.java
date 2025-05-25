70a022703843bcdda4f0648a1095299544dcb6df
EmmanuelPosh101 <emmanuelposholi12@gmail.com>
2025-05-25 20:27:44 +0200

    Update CustomerFactory.java

--- src/main/java/za/co/cinemabookingdomain/factory/CustomerFactory.java
+++ src/main/java/za/co/cinemabookingdomain/factory/CustomerFactory.java
@@ -1,3 +1,9 @@
+/*CustomerFactory.java
+CustomerFactory POJO class
+Author: EP Posholi (222144408)
+Date: 25 May 2025
+ */
+
 package za.co.cinemabookingdomain.factory;
 
 import za.co.cinemabookingdomain.Domain.Customer;
